104c2cc01f6bdeeeb71c73f5057e69c69a31cc91
darluna2024jo <darvinlunaj@gmail.com>
2025-09-24 23:38:51 -0600

    add Icon component with customizable properties

---
+++ src/components/Icon.tsx
@@ -0,0 +1,31 @@
+import React, {FC} from 'react';
+import {StyleSheet, Text, TextStyle} from 'react-native';
+
+import {fss} from '../utils';
+import {NicaQuizz as Types} from '../types/interfaces';
+
+interface IconProps {
+    icon: string;
+    color?: Types.ThemeColorKey;
+    size?: number;
+    style?: TextStyle;
+}
+
+export const Icon: FC<IconProps> = ({icon, color, size, style}) => {
+    const styles = makeStyles(color, size, style);
+    return <Text style={styles.icon}>{icon}</Text>;
+};
+
+const makeStyles = (
+    color?: Types.ThemeColorKey,
+    size?: number,
+    style?: TextStyle,
+) =>
+    StyleSheet.create({
+        icon: {
+            color: color,
+            fontSize: fss(size || 20),
+            fontFamily: 'nova-icons',
+            ...style,
+        },
+    });
